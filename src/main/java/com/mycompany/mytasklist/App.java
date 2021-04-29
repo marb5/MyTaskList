aa22f9950ecccd430ef5184acb2defffc42aaa92
Marcin Bielen <mbielen1995@gmail.com>
2021-04-29 17:28:24 +0200

    Add embedded Jetty

--- src/main/java/com/mycompany/mytasklist/App.java
+++ src/main/java/com/mycompany/mytasklist/App.java
@@ -1,14 +1,50 @@
 package com.mycompany.mytasklist;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //System.out.println("Hello World");
         //Dodanie loggera, sluzocego do wyswietlania danych
-        Logger logger = LoggerFactory.getLogger(App.class);
-        logger.info("Hello World message");
+        /*Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World message");*/
+        
+        WebAppContext webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[] {
+            new AnnotationConfiguration(),
+            new WebInfConfiguration(),
+            new WebXmlConfiguration(),
+            new MetaInfConfiguration(),
+            new FragmentConfiguration(),
+            new EnvConfiguration(),
+            new PlusConfiguration(),
+            new JettyWebXmlConfiguration()
+        });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+        //webapp.addServlet(MyServlet.class, "/api/*");
+        
+        Server server = new Server(8080);
+        server.setHandler(webapp);
+
+        server.start();
+        server.join();
     }
 }
