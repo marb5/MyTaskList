8f39551cb2ebeebc6ffc313b6e95542af7f42dd8
Marcin Bielen <mbielen1995@gmail.com>
2021-04-30 12:25:20 +0200

    Add tests to service

---
+++ src/test/java/com/mycompany/mytasklist/MyServiceTest.java
@@ -0,0 +1,43 @@
+package com.mycompany.mytasklist;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+/**
+ *
+ * @author marcin
+ */
+public class MyServiceTest {
+    //SUT - system under test
+    private MyService SUT = new MyService();
+    
+    @Test
+    public void testDemo() throws Exception {
+        assertTrue(true);
+    }
+    
+    @Test
+    public void testGreeting_null_returnsDefaultValue() throws Exception {
+        //given
+        String name = null;
+        
+        //when
+        String result = SUT.greeting(name);
+        
+        //then
+        assertEquals("world", result);
+    }
+    
+    @Test
+    public void testGreeting_name_returnsName() throws Exception {
+        //given
+        String name = "TwojeImie";
+        
+        //when
+        String result = SUT.greeting(name);
+        
+        //then
+        assertEquals(name, result);
+    }
+}
